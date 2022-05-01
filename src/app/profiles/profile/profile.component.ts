6db0f979d09bd38c35945d223d07b7eadf6fbc29
fernandokvk <tukori2@gmail.com>
2022-05-01 16:51:47 -0300

    Home refactor

--- src/app/profiles/profile/profile.component.ts
+++ src/app/profiles/profile/profile.component.ts
@@ -22,7 +22,7 @@ export class ProfileComponent implements OnInit {
     // @ts-ignore
     // this.activeSession.credential = {id: 15, courierId: 1, type: "COURIER"};
     // this.activeSession.credential = {id: 1, type: "CUSTOMER"};
-    this.activeSession.credential = {id: 15, shopId: 1, type: "SHOP"};
+    // this.activeSession.credential = {id: 15, shopId: 1, type: "SHOP"};
   }
 
   goBack() {
